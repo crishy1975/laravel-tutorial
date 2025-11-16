8860c17a74371c49ea6da1afaf78c55009be10f8
crishy1975 <resch.ohg@gmail.net>
2025-11-16 09:40:31 +0100

    use

--- app/Models/Rechnung.php
+++ app/Models/Rechnung.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\Models\Gebaeude;
 
 class Rechnung extends Model
 {
