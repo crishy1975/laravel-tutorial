747bfac0da167143ce45ca34958d60b618042a90
crishy1975 <resch.ohg@gmail.net>
2025-12-24 06:56:45 +0100

    Auto-Backup 24.12.2025 06:56

---
+++ START-Auto-Commit.bat
@@ -0,0 +1,5 @@
+@echo off
+title Git Auto-Commit - UschiWeb
+cd /d "C:\Users\Christian\Documents\entwicklung\laravel-tutorial"
+powershell -ExecutionPolicy Bypass -File "C:\Users\Christian\Documents\entwicklung\laravel-tutorial\git-auto-commit.ps1" -Minutes 1
+pause
