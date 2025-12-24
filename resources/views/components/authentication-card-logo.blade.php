5fdcacbd858aebc6d60f46b398737a5eaf7ed642
crishy1975 <resch.ohg@gmail.net>
2025-12-24 08:29:56 +0100

    Auto-Backup 24.12.2025 08:29

--- resources/views/components/authentication-card-logo.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Zeigt Haus-Icon + Wortmarke im Login-Header --}}
-<a href="{{ url('/') }}" class="d-inline-flex align-items-center gap-2 text-decoration-none">
-  {{-- Haus-Icon --}}
-  <i class="bi bi-house-door-fill" aria-hidden="true" style="font-size: 2rem;"></i>
-  {{-- Wortmarke (anpassbar) --}}
-  <span class="fw-semibold text-dark">Resch GmbH – UschiWeb</span>
-</a>
