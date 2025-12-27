fdbe1d8b09f0e1819e3444a1f19e893a34dcde51
crishy1975 <resch.ohg@gmail.net>
2025-12-27 11:32:22 +0100

    Auto-Backup 27.12.2025 11:32

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    $this->comment(\Illuminate\Foundation\Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Fälligkeiten täglich um 02:30
+Schedule::command('faelligkeit:recalc-all')
+    ->dailyAt('02:30')
+    ->timezone('Europe/Rome');
+
+// Backup alle 5 Minuten (Test) - später auf weekly ändern!
+Schedule::command('backup:create --force')
+    ->everyFiveMinutes()
+    ->timezone('Europe/Rome');
