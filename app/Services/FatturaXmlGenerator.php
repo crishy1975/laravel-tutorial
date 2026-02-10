8e145094b923c3228eeb0a90bc4b49f97eb13c95
crishy1975 <resch.ohg@gmail.net>
2026-02-10 10:59:37 +0100

    Auto-Backup 10.02.2026 10:59

--- app/Services/FatturaXmlGenerator.php
+++ app/Services/FatturaXmlGenerator.php
@@ -760,6 +760,8 @@ class FatturaXmlGenerator
             // 8. RiferimentoNormativo - MUSS am Ende stehen!
             if ($satz == 0 && $this->rechnung->reverse_charge) {
                 $this->addElement('RiferimentoNormativo', $riepilogo, 'Non imponibile art. 17, c.6, lett. a-ter) DPR 633/72');
+            } elseif ($this->rechnung->split_payment) {
+                $this->addElement('RiferimentoNormativo', $riepilogo, 'Splitpayment ex art. 17-ter DPR 633/72');
             }
         }
     }
