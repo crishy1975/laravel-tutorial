50e3979a205e8e72fb744c0bf277fcdffb0baa47
crishy1975 <resch.ohg@gmail.net>
2026-02-10 10:54:36 +0100

    Auto-Backup 10.02.2026 10:54

--- app/Services/FatturaXmlGenerator.php
+++ app/Services/FatturaXmlGenerator.php
@@ -759,7 +759,7 @@ class FatturaXmlGenerator
 
             // 8. RiferimentoNormativo - MUSS am Ende stehen!
             if ($satz == 0 && $this->rechnung->reverse_charge) {
-                $this->addElement('RiferimentoNormativo', $riepilogo, 'Inversione contabile art. 17 DPR 633/72');
+                $this->addElement('RiferimentoNormativo', $riepilogo, 'Non imponibile art. 17, c.6, lett. a-ter) DPR 633/72');
             }
         }
     }
