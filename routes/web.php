6c027db381c2103f88d0cab6f4a4c4a7f98158da
crishySarnthein <resch.ohg@gmail.com>
2026-03-14 11:26:05 +0100

    Auto-Backup 14.03.2026 11:26

--- routes/web.php
+++ routes/web.php
@@ -320,7 +320,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
         Route::post('/{buchung}/match', [BankBuchungController::class, 'match'])->name('match');
         Route::delete('/{buchung}/unmatch', [BankBuchungController::class, 'unmatch'])->name('unmatch');
         Route::post('/{buchung}/ignore', [BankBuchungController::class, 'ignore'])->name('ignore');
-        Route::post('/{buchung}/verify', [BankController::class, 'toggleVerify'])->name('bank.verify');
+        Route::post('/{buchung}/verify', [BankBuchungController::class, 'toggleVerify'])->name('bank.verify');
     });
 
 
