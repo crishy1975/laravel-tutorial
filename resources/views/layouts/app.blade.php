289cbedd530d1c2174e0189c9618bd3760779eaf
crishy1975 <resch.ohg@gmail.com>
2026-08-26 07:14:37 +0200

    Auto-Backup 26.08.2026 07:14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@ PFAD:  resources/views/layouts/app.blade.php
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet">
 
     {{-- Livewire Styles --}}
     @livewireStyles
